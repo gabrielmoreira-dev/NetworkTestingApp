30a96d9da08ec588cb60bf43b1ddb88b4b901f06
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-07-27 00:49:00 -0300

    Initial Commit

---
+++ NetworkTestingApp/NetworkTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTestingAppApp.swift
+//  NetworkTestingApp
+//
+//  Created by Gabriel Moreira on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
